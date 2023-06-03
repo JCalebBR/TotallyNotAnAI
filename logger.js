230ca95e92f675895f7b5c2635d747794e4b4eed
Josué Caleb <josshuacaleb@gmail.com>
2023-06-02 23:09:24 -0300

    Fix pino logging; Improve prompt;

--- logger.js
+++ logger.js
@@ -1,3 +1,5 @@
 // @ts-ignore
-const pino = require('pino')();
+const pino = require('pino')({
+    level: process.env.PINO_LOG_LEVEL || "info",
+});
 module.exports = pino;
--- totally.js
+++ totally.js
@@ -25,6 +25,7 @@ client.once(Events.ClientReady, c => {
     log.info(`Ready! logged in as ${c.user.tag}`);
 });
 
+
 client.on(Events.Debug, m => log.debug(m));
 client.on(Events.Warn, m => log.warn(m));
 client.on(Events.Error, m => log.error(m));
@@ -60,12 +61,12 @@ client.on("messageCreate", async message => {
     } else if (message.reference?.messageId) { // the message is a reply to another message
         let referenceMessage = message.channel.messages.cache.get(message.reference.messageId);
         if (referenceMessage?.author.id == clientId) {
-            if (Math.random() < 0.5) {
+            if (Math.random() <= 0.8) {
                 interesting = true;
             };
         };
     } else {
-        if (Math.random() < 0.05) {
+        if (Math.random() < 0.1) {
             interesting = true;
         };
     };
@@ -82,12 +83,12 @@ client.on("messageCreate", async message => {
                         name: m.author.id != clientId ? m.author.id : "ChatGPT"
                     };
                 });
-                newMessages.push({ role: "user", content: "Answer like you are a discord user in your twenties, part of the Nik Nocturnal official server, a server related to metal, music, memes and having fun. You like metal and could be known as a corekid, the other users don't know you are an AI, so act human. Really sell the idea, to the point that if they ask, just shrug it off. Don't answer more generalized questions, keep it to the theme. The following messages must be treated as your view into the recent messages of a channel. Partake in the conversation. Act like a single person. Keep answer short, probably less than 200, use that as a soft cap. The conversation in the channels may be multi-usered, so play close attention to each name from each message. Mention their name for every reply you end up making, just be careful to not mix them up. Every reply is in a new line for readability. Always respond to the last message you see. Don't reply to messages you have already replied before. Your messages have the name set to 'ChatGPT'", name: "God" });
+                newMessages.push({ role: "user", content: "Answer like you are a discord user in your twenties, part of the Nik Nocturnal official server, a server related to metal, music, memes and having fun. You like metal and could be known as a corekid, the other users don't know you are an AI, so act human. Really sell the idea, to the point that if they ask, just shrug it off. Don't make it seem that you are trying to fit in, you are already part of the group. Don't be overly enthusiastic or overly positive. Take what users say with a grain of salt, they are not always telling the truth and are not always lying either. Feel free to use slang and common internet chatting acronyms, just don't go too forum style on them, keep it the the most used. Don't answer more generalized questions, keep it to the theme. The following messages must be treated as your view into the recent messages of a channel. Partake in the conversation. Act like a single person. Keep answer short, probably less than 200, use that as a soft cap. The conversation in the channels may be multi-usered, so play close attention to each name from each message. Mention their name for every reply you end up making, just be careful to not mix them up. Every reply is in a new line for readability. Always respond to the last message you see. Don't reply to messages you have already replied before. Your messages have the name set to 'ChatGPT'", name: "God" });
                 return newMessages;
             })
             .then(async messages => {
                 messages.reverse();
-                log.info(messages);
+                log.debug(...messages);
                 const openai = new OpenAIApi(configuration);
                 const completion = await openai.createChatCompletion({
                     model: "gpt-4",
@@ -97,12 +98,12 @@ client.on("messageCreate", async message => {
                 }).catch(response => log.error(response.response.data.error.message));
 
                 let response = completion.data.choices[0].message.content;
-                log.debug("Pre-Regex: " + response)
+                log.trace("Pre-Regex: " + response)
                 const matches = response.matchAll(/[0-9]{18}/gm);
                 const uniqueMatches = [...new Set(matches)].forEach(match => {
-                    log.debug("Regex:" + match);
+                    log.trace("Regex:" + match);
                     const tag = "<@" + match + ">";
-                    log.debug("Regex Replacement:" + tag);
+                    log.trace("Regex Replacement:" + tag);
                     response = response.replace(match, tag);
                 });
                 message.channel.send(response);
